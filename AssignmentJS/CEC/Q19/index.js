e16602430806c1fb5e1561f34c4c3c7e3209bf3a
bhosaleshivkanya <bhosaleshivkanya1998@gmail.com>
2025-11-26 19:51:29 +0530

    Create index.js

---
+++ AssignmentJS/CEC/Q19/index.js
@@ -0,0 +1,16 @@
+let age = 20;
+
+function displayAge() {
+  console.log("Age is:", age);
+}
+
+
+function changeAge() {
+  age = 25; 
+  console.log("Updated Age is:", age);
+}
+
+
+displayAge(); 
+changeAge();  
+displayAge(); 
