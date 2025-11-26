193829e3282a2239b39769d510c48e812dec83ea
Shivkanya <bhosaleshivkanya1998@gmail.com>
2025-11-26 19:28:33 +0530

    fix nested repo and add Q19 correctly

---
+++ AssignmentJS/CEC/Q19/index.js
@@ -0,0 +1,18 @@
+
+
+let age = 20;
+
+function displayAge() {
+  console.log("Age is:", age);
+}
+
+
+function changeAge() {
+  age = 25; 
+  console.log("Updated Age is:", age);
+}
+
+
+displayAge(); 
+changeAge();  
+displayAge(); 
